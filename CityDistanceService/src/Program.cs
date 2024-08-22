2eeb7299cd8c7450eef6ddfc305677e6035a5f2f
JimBimBomBom <95131004+JimBimBomBom@users.noreply.github.com>
2024-08-22 17:44:18 +0200

    added cors to program.cs

--- CityDistanceService/src/Program.cs
+++ CityDistanceService/src/Program.cs
@@ -67,7 +67,7 @@ configuration.AddEnvironmentVariables();
     if (configuration["DATABASE_CONNECTION_STRING"] == null)
     {
         Console.WriteLine("DATABASE_CONNECTION_STRING environment variable not set. Missing default value.");
-        Environment.SetEnvironmentVariable("DATABASE_CONNECTION_STRING", "Server=db;Database=CityDistanceService;Uid=root;Pwd=changeme;");
+        Environment.SetEnvironmentVariable("DATABASE_CONNECTION_STRING", "Server=localhost;Database=CityDistanceService;Uid=root;Pwd=changeme;");
     }
 
     var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
@@ -92,6 +92,11 @@ configuration.AddEnvironmentVariables();
 var app = builder.Build();
 
 app.UseRouting();
+
+app.UseCors(builder =>
+    builder.WithOrigins("https://jimbimbombom.github.io")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
 var exemptedPaths = new List<string> { "/health_check", "/db_health_check", "/version", "/distance", "/swagger", "/swagger/index.html" };
 app.UseWhen(
