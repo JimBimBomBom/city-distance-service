b7093c85a29add255a1d7a14dadfe7721b33780c
JimBimBomBom <95131004+JimBimBomBom@users.noreply.github.com>
2024-10-06 10:00:18 +0200

    Release of version 0.1.24

--- CityDistanceService/src/Constants.cs
+++ CityDistanceService/src/Constants.cs
@@ -3,5 +3,5 @@ class Constants
     /// <summary>
     /// The version of the application.
     /// </summary>
-   public const string Version = "0.1.23";
+   public const string Version = "0.1.24";
 }
