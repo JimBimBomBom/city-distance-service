8b51d16c18270826da4f1f43bca889ba9e52c67c
JimBimBomBom <95131004+JimBimBomBom@users.noreply.github.com>
2026-03-09 18:11:36 +0100

    works, now a new version

--- CityDistanceService/src/Constants.cs
+++ CityDistanceService/src/Constants.cs
@@ -3,7 +3,7 @@ class Constants
     /// <summary>
     /// The version of the application.
     /// </summary>
-   public const string Version = "0.3.1";
+   public const string Version = "0.3.2";
 
    // Used for getting localized names - only these languages are fetched
    public static readonly string[] SupportedLanguages = new[] {"en", "cs", "sk", "de", "ru"};
